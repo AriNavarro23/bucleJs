38ad6b340cfc985426b2845a2cadfdde2a17a60d
Ariel Navarro <navarro.a.ariel@hotmail.com>
2022-08-21 15:43:49 -0300

    first

---
+++ Bucle.js
@@ -0,0 +1,58 @@
+// WHILE
+
+/*
+let x = prompt('Enter a number');
+while(x <= 10){
+    console.log(`The numer is ${x}`);
+    x++;
+}
+
+console.log('End while');
+*/
+
+
+
+// DO WHILE ( hacer mientras algo se cumple, se ejecuta al menos una vez)
+
+/*
+let a = prompt('Enter a number');
+
+do{
+    console.log(`the number is ${a}`);
+    a++
+} while(a <= 10);
+
+*/
+
+
+
+// FOR
+
+/*
+for(let i = 0; i <= 10; i++){
+    console.log(`the number is ${i}`);
+}
+
+console.log('End the bucle for')
+*/
+
+
+// FOR OF
+
+/*
+const array = [ "pear", "tomato", "apple"]
+
+for(const element of array){
+    console.log(`i will have buy ${element} at the supermarket`)
+}
+*/
+
+// FOR IN
+
+
+
+// IF
+
+
+
+// IF ELSE
